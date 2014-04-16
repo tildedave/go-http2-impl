e8a1b37a03e3d80b34895a8353985b5f27d2b48a
Dave King <dave.king@rackspace.com>
2014-04-16 19:57:35 -0400

    Initial commit

---
+++ server/server.go
@@ -0,0 +1,5 @@
+package server
+
+func Respond(data string) string {
+	return "dalksgjlak"
+}
---
+++ server/server_test.go
@@ -0,0 +1,7 @@
+package server
+
+import "testing"
+
+func TestRespondMustBeginWithPreface(t *testing.T) {
+	t.Errorf("wubba lubba dub dubs")
+}
