5be1bb8184a70273850a1321e16459da29db69f9
Dave King <dave.king@rackspace.com>
2014-04-20 19:51:28 -0400

    Remove TODOs that have been done or moved to Github issues

--- hpack/header.go
+++ hpack/header.go
@@ -1,8 +1,5 @@
 package hpack
 
-// TODO: headers of arbitrary length with integer encoding algorithm
-// TODO: hpack test cases https://github.com/http2jp/hpack-test-case
-
 func Decode(headers string, table *HeaderTable) ([]HeaderField, int) {
 	var decodedHeaders []HeaderField
 
