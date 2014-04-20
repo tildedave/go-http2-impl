612be9764da96760267522a4267a2a3cfe88f203
Dave King <dave.king@rackspace.com>
2014-04-20 15:54:29 -0400

    String encoding and decoding

---
+++ hpack/string.go
@@ -0,0 +1,13 @@
+package hpack
+
+func encodeLiteral(literal string) string {
+	return encodeInteger(len(literal), 7) + literal
+}
+
+func decodeLiteral(wire *[]byte) string {
+	len := decodeInteger(wire, 7)
+	decoded := (*wire)[0:len]
+	*wire = (*wire)[len:]
+
+	return string(decoded)
+}
