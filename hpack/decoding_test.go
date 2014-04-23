529aae7d94853524f0f55528c34fb18eeac07a51
Dave King <dave.king@rackspace.com>
2014-04-22 21:56:39 -0400

    Add final decoding test from IETF draft

--- hpack/decoding_test.go
+++ hpack/decoding_test.go
@@ -133,4 +133,16 @@ func TestDecodeWithEviction(t *testing.T) {
 		HeaderField{"date", "Mon, 21 Oct 2013 20:13:21 GMT"},
 		HeaderField{"location", "https://www.example.com"},
 	})
+
+	headers, _ = context.Decode("\x84\x84\x43\x1d\x4d\x6f\x6e\x2c\x20\x32\x31\x20\x4f\x63\x74\x20\x32\x30\x31\x33\x20\x32\x30\x3a\x31\x33\x3a\x32\x32\x20\x47\x4d\x54\x5e\x04\x67\x7a\x69\x70\x84\x84\x83\x83\x7b\x38\x66\x6f\x6f\x3d\x41\x53\x44\x4a\x4b\x48\x51\x4b\x42\x5a\x58\x4f\x51\x57\x45\x4f\x50\x49\x55\x41\x58\x51\x57\x45\x4f\x49\x55\x3b\x20\x6d\x61\x78\x2d\x61\x67\x65\x3d\x33\x36\x30\x30\x3b\x20\x76\x65\x72\x73\x69\x6f\x6e\x3d\x31")
+
+	assert.Equal(t, len(headers.Headers), 6)
+	assert.Equal(t, headers.Headers, []HeaderField{
+		HeaderField{"cache-control", "private"},
+		HeaderField{"date", "Mon, 21 Oct 2013 20:13:22 GMT"},
+		HeaderField{"content-encoding", "gzip"},
+		HeaderField{"location", "https://www.example.com"},
+		HeaderField{":status", "200" },
+		HeaderField{"set-cookie", "foo=ASDJKHQKBZXOQWEOPIUAXQWEOIU; max-age=3600; version=1"},
+	})
 }
