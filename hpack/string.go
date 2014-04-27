548228a1808e6942197bca9dfb572466e7f7c3bb
Dave King <dave.king@rackspace.com>
2014-04-27 10:45:27 -0400

    Huffman encoding of literals

--- hpack/string.go
+++ hpack/string.go
@@ -4,6 +4,14 @@ func encodeLiteral(literal string) string {
 	return encodeInteger(len(literal), 7) + literal
 }
 
+func encodeLiteralHuffman(literal string) string {
+	str := EncodeHuffman(literal)
+	lenBytes := []byte(encodeInteger(len(str), 7))
+	lenBytes[0] |= 0x80
+
+	return string(lenBytes) + str
+}
+
 func decodeLiteral(wire *[]byte) string {
 	len := decodeInteger(wire, 7)
 
