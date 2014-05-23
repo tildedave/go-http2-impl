6415a56495caa97ef21d564d0b093d59d673a8f9
Dave King <dave.king@rackspace.com>
2014-05-23 18:57:07 -0400

    Remove mock library, use something dumber.

--- server_test.go
+++ server_test.go
@@ -3,26 +3,29 @@ package main
 import (
 	"testing"
 	"github.com/tildedave/go-http2-impl/frame"
-	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/assert"
 )
 
 type MockConn struct{
-	mock.Mock
+	written []byte
+	closed bool
 }
 
 func (c *MockConn) Close() error {
-	args := c.Mock.Called()
-	return args.Error(0)
+	c.closed = true
+	return nil
 }
 
 func (c *MockConn) Write(b []byte) (int, error) {
-	args := c.Mock.Called(b)
-	return args.Int(0), args.Error(1)
+	c.written = append(c.written, b...)
+	return len(b), nil
 }
 
 
 func NewTestServer() (Server, *MockConn) {
 	conn := new(MockConn)
+	conn.written = make([]byte, 0)
+
 	s := Server{ conn }
 
 	return s, conn
@@ -34,12 +37,10 @@ func TestRespondWithoutPreface(t *testing.T) {
 	f := frame.GOAWAY{0, 1, "Did not include connection preface"}
 	bytes := f.Marshal()
 
-	conn.On("Close").Return(nil)
-	conn.On("Write", bytes).Return(len(bytes), nil)
-
 	server.Respond("not the preface")
 
-	conn.Mock.AssertExpectations(t)
+	assert.Equal(t, conn.written, bytes)
+	assert.True(t, conn.closed, "Should have closed the connection")
 }
 
 func TestRespondWithThePreface(t *testing.T) {
@@ -47,9 +48,9 @@ func TestRespondWithThePreface(t *testing.T) {
 
 	// also needs to write settings frame too.
 	preface := "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
-	conn.On("Write", []byte(preface)).Return(len(preface), nil)
 
 	server.Respond("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")
 
-	conn.Mock.AssertExpectations(t)
+	assert.Equal(t, conn.written, []byte(preface))
+	assert.False(t, conn.closed)
 }
