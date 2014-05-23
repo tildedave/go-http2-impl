dbc336969572390a9ecf02b2a377ad11479616dc
Dave King <dave.king@rackspace.com>
2014-05-23 19:29:51 -0400

    Create explicit InitiateConn function

--- server.go
+++ server.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"github.com/tildedave/go-http2-impl/frame"
+	"bufio"
+	"strings"
 )
 
 type Conn interface {
+	Read(b []byte) (n int, err error)
 	Write(b []byte) (n int, err error)
 	Close() error
 }
 
 type Server struct {
-	conn Conn
 }
 
 const preface = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
 
-func (s *Server) Respond(wire string) {
-	if wire != preface {
-		f := frame.GOAWAY{0, 1, "Did not include connection preface"}
-		s.conn.Write(f.Marshal())
-		s.conn.Close()
-		return
+func (s *Server) InitiateConn(conn Conn) error {
+	scanner := bufio.NewScanner(conn)
+	stopped := scanner.Scan()
+	str := ""
+
+	if stopped != false {
+		str += scanner.Text() + "\r\n"
+		if !strings.HasPrefix(preface, str) {
+			f := frame.GOAWAY{0, 1, "Did not include connection preface"}
+			conn.Write(f.Marshal())
+			conn.Close()
+			return nil
+		}
 	}
 
-	s.conn.Write([]byte(preface))
+	conn.Write([]byte(preface))
 	// TODO: SETTINGS frame
+
+	return nil;
 }
--- server_test.go
+++ server_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 type MockConn struct{
+	readData string
 	written []byte
 	closed bool
 }
@@ -14,6 +15,12 @@ type MockConn struct{
 func (c *MockConn) Close() error {
 	c.closed = true
 	return nil
+}
+
+func (c *MockConn) Read(b []byte) (int, error) {
+	n := copy(b, []byte(c.readData))
+	c.readData = ""
+	return n, nil
 }
 
 func (c *MockConn) Write(b []byte) (int, error) {
@@ -26,18 +33,19 @@ func NewTestServer() (Server, *MockConn) {
 	conn := new(MockConn)
 	conn.written = make([]byte, 0)
 
-	s := Server{ conn }
+	s := Server{}
 
 	return s, conn
 }
 
-func TestRespondWithoutPreface(t *testing.T) {
+func TestInitiateConnWithoutPreface(t *testing.T) {
 	server, conn := NewTestServer()
 
 	f := frame.GOAWAY{0, 1, "Did not include connection preface"}
 	bytes := f.Marshal()
 
-	server.Respond("not the preface")
+	conn.readData = "not the preface"
+	server.InitiateConn(conn)
 
 	assert.Equal(t, conn.written, bytes)
 	assert.True(t, conn.closed, "Should have closed the connection")
@@ -49,7 +57,8 @@ func TestRespondWithThePreface(t *testing.T) {
 	// also needs to write settings frame too.
 	preface := "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
 
-	server.Respond("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")
+	conn.readData = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
+	server.InitiateConn(conn)
 
 	assert.Equal(t, conn.written, []byte(preface))
 	assert.False(t, conn.closed)
