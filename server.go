157d256bc54e9fe37a8b63bb0724a66c9c655c90
Dave King <dave.king@rackspace.com>
2014-05-26 15:28:04 -0400

    Add 'conn' struct for managing HTTP/2 connection on server

--- server.go
+++ server.go
@@ -11,14 +11,24 @@ import (
 
 var _ = fmt.Printf // package fmt is now used
 
-type Server struct {
-	EncodingContext *hpack.EncodingContext
+// Internal structure for managing a server connection
+// Connections own streams
+type conn struct {
+	context *hpack.EncodingContext
+	ioc     net.Conn
+}
+
+func newConn(ioc net.Conn) conn {
+	return conn{
+		context: hpack.NewEncodingContext(),
+		ioc:     ioc,
+	}
 }
 
 const preface = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
 
-func (s *Server) InitiateConn(conn net.Conn) error {
-	scanner := bufio.NewScanner(conn)
+func (c *conn) serve() error {
+	scanner := bufio.NewScanner(c.ioc)
 	str := ""
 
 	// TODO: connection upgrade from HTTP 1.0
@@ -26,8 +36,9 @@ func (s *Server) InitiateConn(conn net.Conn) error {
 		str += scanner.Text() + "\r\n"
 		if !strings.HasPrefix(preface, str) {
 			f := GOAWAY{0, 1, "Did not include connection preface"}
-			conn.Write(f.Marshal())
-			conn.Close()
+			c.ioc.Write(f.Marshal())
+			c.ioc.Close()
+
 			return nil
 		}
 
@@ -36,8 +47,7 @@ func (s *Server) InitiateConn(conn net.Conn) error {
 		}
 	}
 
-	conn.Write([]byte(preface))
-	s.EncodingContext = hpack.NewEncodingContext()
+	c.ioc.Write([]byte(preface))
 	// TODO: SETTINGS frame
 
 	return nil
--- server_test.go
+++ server_test.go
@@ -56,79 +56,79 @@ func newFakeConn() *fakeConn {
 	return conn
 }
 
-func NewTestServer() (Server, *fakeConn) {
-	conn := newFakeConn()
-	s := Server{}
+func newTestConn() (conn, *fakeConn) {
+	ioc := newFakeConn()
+	conn := newConn(ioc)
 
-	return s, conn
+	return conn, ioc
 }
 
 func TestInitiateConnWithoutPreface(t *testing.T) {
-	server, conn := NewTestServer()
+	conn, fakeConn := newTestConn()
 
 	f := GOAWAY{0, 1, "Did not include connection preface"}
 	bytes := f.Marshal()
 
-	conn.readData = [][]byte{[]byte("not the preface")}
-	server.InitiateConn(conn)
+	fakeConn.readData = [][]byte{[]byte("not the preface")}
+	conn.serve()
 
-	assert.Equal(t, conn.written, bytes)
-	assert.True(t, conn.closed, "Should have closed the connection")
+	assert.Equal(t, fakeConn.written, bytes)
+	assert.True(t, fakeConn.closed, "Should have closed the connection")
 }
 
 func TestRespondWithThePreface(t *testing.T) {
-	server, conn := NewTestServer()
+	conn, fakeConn := newTestConn()
 
 	// also needs to write settings frame too.
 	preface := "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
 
-	conn.readData = [][]byte{[]byte("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")}
-	server.InitiateConn(conn)
+	fakeConn.readData = [][]byte{[]byte("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")}
+	conn.serve()
 
-	assert.Equal(t, conn.written, []byte(preface))
-	assert.False(t, conn.closed)
+	assert.Equal(t, fakeConn.written, []byte(preface))
+	assert.False(t, fakeConn.closed)
 }
 
 func TestFrameScannerReturnsAFrame(t *testing.T) {
-	conn := newFakeConn()
+	fakeConn := newFakeConn()
 	b := PING{OpaqueData: 3957102}.Marshal()
-	conn.readData = [][]byte{b}
+	fakeConn.readData = [][]byte{b}
 
-	s := NewFrameScanner(conn)
+	s := NewFrameScanner(fakeConn)
 
 	assert.True(t, s.Scan())
 	assert.Equal(t, s.Bytes(), b)
 }
 
 func TestFrameScanner_IncompleteFrame(t *testing.T) {
-	conn := newFakeConn()
+	fakeConn := newFakeConn()
 	b := PING{OpaqueData: 3957102}.Marshal()
-	conn.readData = [][]byte{b[0 : len(b)-1]}
+	fakeConn.readData = [][]byte{b[0 : len(b)-1]}
 
-	s := NewFrameScanner(conn)
+	s := NewFrameScanner(fakeConn)
 
 	assert.False(t, s.Scan())
 }
 
 func TestFrameScanner_IncompleteFrameThatIsLaterCompleted(t *testing.T) {
-	conn := newFakeConn()
+	fakeConn := newFakeConn()
 	b := PING{OpaqueData: 3957102}.Marshal()
-	conn.readData = [][]byte{b[0 : len(b)-1], b[len(b)-1:]}
+	fakeConn.readData = [][]byte{b[0 : len(b)-1], b[len(b)-1:]}
 
-	s := NewFrameScanner(conn)
+	s := NewFrameScanner(fakeConn)
 
 	assert.True(t, s.Scan())
 	assert.Equal(t, s.Bytes(), b)
 }
 
 func TestFrameScanner_TwoFrames(t *testing.T) {
-	conn := newFakeConn()
+	fakeConn := newFakeConn()
 	b1 := PING{OpaqueData: 3957102}.Marshal()
 	b2 := PING{OpaqueData: 12311}.Marshal()
 
-	conn.readData = [][]byte{b1, b2}
+	fakeConn.readData = [][]byte{b1, b2}
 
-	s := NewFrameScanner(conn)
+	s := NewFrameScanner(fakeConn)
 
 	assert.True(t, s.Scan())
 	assert.Equal(t, s.Bytes(), b1)
@@ -138,13 +138,13 @@ func TestFrameScanner_TwoFrames(t *testing.T) {
 }
 
 func TestFrameScanner_TwoFramesCombined(t *testing.T) {
-	conn := newFakeConn()
+	fakeConn := newFakeConn()
 	b1 := PING{OpaqueData: 3957102}.Marshal()
 	b2 := PING{OpaqueData: 12311}.Marshal()
 
-	conn.readData = [][]byte{append(b1, b2...)}
+	fakeConn.readData = [][]byte{append(b1, b2...)}
 
-	s := NewFrameScanner(conn)
+	s := NewFrameScanner(fakeConn)
 
 	assert.True(t, s.Scan())
 	assert.Equal(t, s.Bytes(), b1)
@@ -154,13 +154,13 @@ func TestFrameScanner_TwoFramesCombined(t *testing.T) {
 }
 
 func TestFrameScanner_TwoFrames_Uneven(t *testing.T) {
-	conn := newFakeConn()
+	fakeConn := newFakeConn()
 	b1 := PING{OpaqueData: 3957102}.Marshal()
 	b2 := PING{OpaqueData: 12311}.Marshal()
 
-	conn.readData = [][]byte{b1[0:13], append(b1[13:], b2...)}
+	fakeConn.readData = [][]byte{b1[0:13], append(b1[13:], b2...)}
 
-	s := NewFrameScanner(conn)
+	s := NewFrameScanner(fakeConn)
 
 	assert.True(t, s.Scan())
 	assert.Equal(t, s.Bytes(), b1)
