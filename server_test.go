228e635d72c60556bb4779a51d14a15a3cda5d20
Dave King <dave.king@rackspace.com>
2014-05-26 12:35:53 -0400

    Add a more pathological test case to ensure io.Reader is doing what I want

--- server_test.go
+++ server_test.go
@@ -118,3 +118,19 @@ func TestFrameScanner_TwoFrames(t *testing.T) {
 	assert.Equal(t, s.Bytes(), b2)
 	assert.False(t, s.Scan())
 }
+
+func TestFrameScanner_TwoFrames_Uneven(t *testing.T) {
+	conn := NewMockConn()
+	b1 := PING{OpaqueData: 3957102}.Marshal()
+	b2 := PING{OpaqueData: 12311}.Marshal()
+
+	conn.readData = [][]byte{b1[0:13], append(b1[13:], b2...)}
+
+	s := NewFrameScanner(conn)
+
+	assert.True(t, s.Scan())
+	assert.Equal(t, s.Bytes(), b1)
+	assert.True(t, s.Scan())
+	assert.Equal(t, s.Bytes(), b2)
+	assert.False(t, s.Scan())
+}
